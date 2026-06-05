c245004a9a5c973acc89483ba4515c5c4536fe17
Jaco-Horse-Rides <jacohorserides1@gmail.com>
2026-06-05 04:52:23 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,35 @@
+```javascript
+/* ========================= */
+/* 🔥 NAVBAR SCROLL */
+/* ========================= */
+
+window.addEventListener("scroll", () => {
+
+  const navbar =
+  document.querySelector(".navbar");
+
+  if(window.scrollY > 50){
+
+    navbar.classList.add("scrolled");
+
+  }else{
+
+    navbar.classList.remove("scrolled");
+
+  }
+
+});
+
+/* ========================= */
+/* 🔥 FUTURAS ANIMACIONES */
+/* ========================= */
+
+// Aquí iremos agregando:
+//
+// Slider Tours
+// Galería Premium
+// Reviews
+// FAQ
+// Animaciones Scroll
+// WhatsApp Floating Button
+```
